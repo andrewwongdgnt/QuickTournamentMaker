2c5ab62245322228295eba9a3139784972472249
Andrew Wong <andrew.wong.dgnt@gmail.com>
2020-07-14 16:30:56 -0700

    Initial pass at remaking this whole thing with kotlin and new libraries

--- app/src/main/java/com/dgnt/quickTournamentMaker/model/IKeyable.java
+++
@@ -1,9 +0,0 @@
-package com.dgnt.quickTournamentMaker.model;
-
-/**
- * Created by Owner on 3/13/2016.
- */
-public interface IKeyable {
-
-    public String getKey();
-}
--- app/src/main/java/com/dgnt/quickTournamentMaker/model/tournament/SingleMatchUpTournament.java
+++
@@ -1,7 +0,0 @@
-package com.dgnt.quickTournamentMaker.model.tournament;
-
-/**
- * Created by Owner on 9/15/2016.
- */
-public interface SingleMatchUpTournament {
-}
