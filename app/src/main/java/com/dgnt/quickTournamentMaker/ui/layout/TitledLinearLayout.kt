f35aac15a8310d6063e4f2b02482077b05ac4df7
Andrew Wong <andrew.wong.dgnt@gmail.com>
2020-11-21 03:33:09 -0800

    added a help icon beside the title in titledLinearLayout added a numberpicker from  'com.shawnlin:number-picker:2.4.11'

--- app/src/main/java/com/dgnt/quickTournamentMaker/ui/layout/TitledLinearLayout.kt
+++ app/src/main/java/com/dgnt/quickTournamentMaker/ui/layout/TitledLinearLayout.kt
@@ -2,8 +2,11 @@ package com.dgnt.quickTournamentMaker.ui.layout
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.dgnt.quickTournamentMaker.R
 
 
@@ -12,12 +15,14 @@ class TitledLinearLayout @JvmOverloads constructor(context: Context, attrs: Attr
     init {
         inflate(context, R.layout.title_text_view, this)
 
-        val textView = findViewById<TextView>(R.id.titleOfGroup)
         val padding = 10
         setPadding(padding,padding,padding,padding)
-        background = context.getDrawable(R.drawable.titled_linear_layout_background)
+        background = ContextCompat.getDrawable(context,R.drawable.titled_linear_layout_background)
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.TitledLinearLayout)
-        textView.text = attributes.getString(R.styleable.TitledLinearLayout_titleText)
+        val titleOfGroup = findViewById<TextView>(R.id.titleOfGroup_tv)
+        titleOfGroup.text = attributes.getString(R.styleable.TitledLinearLayout_titleText)
+        val help = findViewById<ImageView>(R.id.help_iv)
+        help.visibility = if (attributes.getBoolean(R.styleable.TitledLinearLayout_showHelp,false)) View.VISIBLE else View.GONE
         attributes.recycle()
 
     }
