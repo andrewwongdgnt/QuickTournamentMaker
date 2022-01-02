4d9d10de92d6d7d708d9729e35f923c245cbc2d0
Andrew Wong <andrew.wong.dgnt@gmail.com>
2022-01-02 15:16:01 -0800

    Continue adding more test assertions into the test file

--- app/src/test/java/com/dgnt/quickTournamentMaker/TournamentDataTransformerServiceTest.kt
+++ app/src/test/java/com/dgnt/quickTournamentMaker/TournamentDataTransformerServiceTest.kt
@@ -1,8 +1,12 @@
 package com.dgnt.quickTournamentMaker
 
+import com.dgnt.quickTournamentMaker.Data.Companion.ANDREW_PERSON
 import com.dgnt.quickTournamentMaker.model.tournament.*
 import com.dgnt.quickTournamentMaker.service.implementation.TournamentDataTransformerService
-import com.dgnt.quickTournamentMaker.service.interfaces.*
+import com.dgnt.quickTournamentMaker.service.interfaces.IMatchUpStatusTransformService
+import com.dgnt.quickTournamentMaker.service.interfaces.IRankingConfigService
+import com.dgnt.quickTournamentMaker.service.interfaces.IRankingService
+import com.dgnt.quickTournamentMaker.service.interfaces.IRoundUpdateService
 import org.joda.time.LocalDateTime
 import org.junit.Assert
 import org.junit.Before
@@ -11,24 +15,62 @@ import org.powermock.api.mockito.PowerMockito
 
 class TournamentDataTransformerServiceTest {
 
+    private val PARTICIPANT_TITLE = "participantTitle"
+    private val PARTICIPANT_NOTE = "participantNote"
+    private val PARTICIPANT_COLOR = 4321
+
+    private val MATCH_UP_TITLE = "matchUpTitle"
+    private val MATCH_UP_NOTE = "matchUpNote"
+    private val MATCH_UP_COLOR = 1234
+
+    private val ROUND_ORIGINAL_TITLE = "roundOriginalTitle"
+    private val ROUND_TITLE = "roundTitle"
+    private val ROUND_NOTE = "roundNote"
+    private val ROUND_COLOR = -666
+
+    private val TOURNAMENT_TITLE = "tournamentTitle"
+    private val TOURNAMENT_DESCRIPTION = "tournamentDescription"
+
     private val mockRankingConfigService = PowerMockito.mock(IRankingConfigService::class.java)
     private val mockRoundUpdateService = PowerMockito.mock(IRoundUpdateService::class.java)
     private val mockRankingService = PowerMockito.mock(IRankingService::class.java)
     private val mockMatchUpStatusTransformService = PowerMockito.mock(IMatchUpStatusTransformService::class.java)
     private val sut = TournamentDataTransformerService(mockRankingConfigService)
 
-    private lateinit var simpleTournament: Tournament
+    private val CREATION_TIME = 555444L
+
+    private lateinit var tournament1: Tournament
 
     @Before
     fun setUp() {
         val round1 = Round(
-            0, 0, listOf(
-                MatchUp(0, 0, 0, Data.ANDREW, Data.KYRA, ""),
-                MatchUp(0, 0, 1, Data.DGNT, Data.KELSEY, ""),
+            0, 0,
+            listOf(
+                MatchUp(
+                    0,
+                    0,
+                    0,
+                    Participant(
+                        ANDREW_PERSON,
+                        name = PARTICIPANT_TITLE,
+                        note = PARTICIPANT_NOTE,
+                        color = PARTICIPANT_COLOR,
+                    ),
+                    Data.KYRA,
+                    MATCH_UP_TITLE,
+                    true,
+                    MatchUpStatus.P1_WINNER,
+                    MATCH_UP_NOTE,
+                    MATCH_UP_COLOR
+                ),
+                MatchUp(0, 0, 1, Data.DGNT, Data.KELSEY, "", status = MatchUpStatus.P2_WINNER),
                 MatchUp(0, 0, 1, Data.FIRE, Participant.BYE_PARTICIPANT, ""),
                 MatchUp(0, 0, 2, Data.HERO, Participant.BYE_PARTICIPANT, "")
             ),
-            ""
+            ROUND_ORIGINAL_TITLE,
+            ROUND_TITLE,
+            ROUND_NOTE,
+            ROUND_COLOR,
         )
         val round2 = Round(
             0, 1,
@@ -56,8 +98,8 @@ class TournamentDataTransformerServiceTest {
             ""
         )
         val roundGroups = listOf(RoundGroup(0, listOf(round1, round2), ""), RoundGroup(1, listOf(round1_2, round2_2), ""))
-        simpleTournament = Tournament(
-            TournamentInformation("title", "description", TournamentType.ELIMINATION, SeedType.CUSTOM, RankPriorityConfig.DEFAULT, LocalDateTime.now()),
+        tournament1 = Tournament(
+            TournamentInformation(TOURNAMENT_TITLE, TOURNAMENT_DESCRIPTION, TournamentType.ELIMINATION, SeedType.CUSTOM, RankPriorityConfig.DEFAULT, LocalDateTime(CREATION_TIME)),
             roundGroups,
             mockMatchUpStatusTransformService,
             mockRoundUpdateService,
@@ -66,11 +108,15 @@ class TournamentDataTransformerServiceTest {
     }
 
 
-
     @Test
     fun testTransform() {
-        val tournamentData = sut.transform(simpleTournament)
-        Assert.assertEquals("title", tournamentData.title)
+        val tournamentData = sut.transform(tournament1)
+        Assert.assertEquals(TOURNAMENT_TITLE, tournamentData.title)
+        Assert.assertEquals(TOURNAMENT_DESCRIPTION, tournamentData.description)
+        Assert.assertEquals(TournamentType.ELIMINATION, tournamentData.type)
+        Assert.assertNull(tournamentData.rankingConfig)
+        Assert.assertEquals(SeedType.CUSTOM, tournamentData.seedType)
+        Assert.assertEquals(CREATION_TIME, tournamentData.creationDate)
     }
 
 
