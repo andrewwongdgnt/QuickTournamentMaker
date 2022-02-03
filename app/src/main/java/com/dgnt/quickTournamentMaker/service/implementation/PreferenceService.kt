1fdd7326048144de2797277b66a4927f3a5a7fc1
Andrew Wong <andrew.wong.dgnt@gmail.com>
2022-02-03 00:27:12 -0800

    refactor code in PreferenceService so that it isn't repeating so much code

--- app/src/main/java/com/dgnt/quickTournamentMaker/service/implementation/PreferenceService.kt
+++ app/src/main/java/com/dgnt/quickTournamentMaker/service/implementation/PreferenceService.kt
@@ -43,82 +43,95 @@ class PreferenceService(private val sharedPreferences: SharedPreferences, privat
 
     }
 
-    override fun isRankingBasedOnPriority(tournamentType: TournamentType): Boolean =
-        when (tournamentType) {
-            TournamentType.SWISS -> sharedPreferences.getBoolean(PREF_SWISS_RANKING_CONFIG_KEY, true)
-            TournamentType.ROUND_ROBIN -> sharedPreferences.getBoolean(PREF_ROUND_ROBIN_RANKING_CONFIG_KEY, true)
-            else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
+    override fun isRankingBasedOnPriority(tournamentType: TournamentType) =
+        sharedPreferences.getBoolean(
+            when (tournamentType) {
+                TournamentType.SWISS -> PREF_SWISS_RANKING_CONFIG_KEY
+                TournamentType.ROUND_ROBIN -> PREF_ROUND_ROBIN_RANKING_CONFIG_KEY
+                else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
+            }, true
+        )
+
+
+    override fun setRankingBasedOnPriority(tournamentType: TournamentType, value: Boolean) =
+        sharedPreferences.edit().run {
+            putBoolean(
+                when (tournamentType) {
+                    TournamentType.SWISS -> PREF_SWISS_RANKING_CONFIG_KEY
+                    TournamentType.ROUND_ROBIN -> PREF_ROUND_ROBIN_RANKING_CONFIG_KEY
+                    else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
+                }, value
+            )
+            apply()
         }
 
-
-    override fun setRankingBasedOnPriority(tournamentType: TournamentType, value: Boolean) {
-        val editor = sharedPreferences.edit()
-        when (tournamentType) {
-            TournamentType.SWISS -> editor.putBoolean(PREF_SWISS_RANKING_CONFIG_KEY, value)
-            TournamentType.ROUND_ROBIN -> editor.putBoolean(PREF_ROUND_ROBIN_RANKING_CONFIG_KEY, value)
-            else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
-        }
-        editor.apply()
-    }
-
-    override fun getRankPriority(tournamentType: TournamentType): RankPriorityConfig =
+    override fun getRankPriority(tournamentType: TournamentType) =
         rankingConfigService.buildRankingFromPriority(
-            when (tournamentType) {
-                TournamentType.SWISS -> sharedPreferences.getString(PREF_SWISS_RANK_PRIORITY_KEY, RankPriorityConfig.DEFAULT_INPUT) ?: RankPriorityConfig.DEFAULT_INPUT
-                TournamentType.ROUND_ROBIN -> sharedPreferences.getString(PREF_ROUND_ROBIN_RANK_PRIORITY_KEY, RankPriorityConfig.DEFAULT_INPUT) ?: RankPriorityConfig.DEFAULT_INPUT
-                else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
-            }
+            sharedPreferences.getString(
+                when (tournamentType) {
+                    TournamentType.SWISS -> PREF_SWISS_RANK_PRIORITY_KEY
+                    TournamentType.ROUND_ROBIN -> PREF_ROUND_ROBIN_RANK_PRIORITY_KEY
+                    else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
+                }, RankPriorityConfig.DEFAULT_INPUT
+            ) ?: RankPriorityConfig.DEFAULT_INPUT
         )
 
 
-    override fun setRankPriority(tournamentType: TournamentType, rankPriorityConfig: RankPriorityConfig) {
-        val editor = sharedPreferences.edit()
-        editor.putString(
-            when (tournamentType) {
-                TournamentType.SWISS -> PREF_SWISS_RANK_PRIORITY_KEY
-                TournamentType.ROUND_ROBIN -> PREF_ROUND_ROBIN_RANK_PRIORITY_KEY
-                else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
-            }, rankingConfigService.toString(rankPriorityConfig)
-        )
-        editor.apply()
+    override fun setRankPriority(tournamentType: TournamentType, rankPriorityConfig: RankPriorityConfig) =
+        sharedPreferences.edit().run {
+            putString(
+                when (tournamentType) {
+                    TournamentType.SWISS -> PREF_SWISS_RANK_PRIORITY_KEY
+                    TournamentType.ROUND_ROBIN -> PREF_ROUND_ROBIN_RANK_PRIORITY_KEY
+                    else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
+                }, rankingConfigService.toString(rankPriorityConfig)
+            )
+            apply()
+        }
 
-    }
-
-    override fun getRankScore(tournamentType: TournamentType): RankScoreConfig =
+    override fun getRankScore(tournamentType: TournamentType) =
         rankingConfigService.buildRankingFromScore(
-            when (tournamentType) {
-                TournamentType.SWISS -> sharedPreferences.getString(PREF_SWISS_RANK_SCORE_KEY, RankScoreConfig.DEFAULT_INPUT) ?: RankScoreConfig.DEFAULT_INPUT
-                TournamentType.ROUND_ROBIN -> sharedPreferences.getString(PREF_ROUND_ROBIN_RANK_SCORE_KEY, RankScoreConfig.DEFAULT_INPUT) ?: RankScoreConfig.DEFAULT_INPUT
-                else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
-            }
+            sharedPreferences.getString(
+                when (tournamentType) {
+                    TournamentType.SWISS -> PREF_SWISS_RANK_SCORE_KEY
+                    TournamentType.ROUND_ROBIN -> PREF_ROUND_ROBIN_RANK_SCORE_KEY
+                    else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
+                }, RankScoreConfig.DEFAULT_INPUT
+            ) ?: RankScoreConfig.DEFAULT_INPUT
         )
 
 
-    override fun setRankScore(tournamentType: TournamentType, rankScoreConfig: RankScoreConfig) {
-        val editor = sharedPreferences.edit()
-        editor.putString(
-            when (tournamentType) {
-                TournamentType.SWISS -> PREF_SWISS_RANK_SCORE_KEY
-                TournamentType.ROUND_ROBIN -> PREF_ROUND_ROBIN_RANK_SCORE_KEY
-                else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
-            }, rankingConfigService.toString(rankScoreConfig)
-        )
-        editor.apply()
-    }
+    override fun setRankScore(tournamentType: TournamentType, rankScoreConfig: RankScoreConfig) =
+        sharedPreferences.edit().run {
+            putString(
+                when (tournamentType) {
+                    TournamentType.SWISS -> PREF_SWISS_RANK_SCORE_KEY
+                    TournamentType.ROUND_ROBIN -> PREF_ROUND_ROBIN_RANK_SCORE_KEY
+                    else -> throw IllegalArgumentException("Tournament Type not supported $tournamentType")
+                }, rankingConfigService.toString(rankScoreConfig)
+            )
+            apply()
+        }
+
 
     override fun getSort() =
-        (sharedPreferences.getString(PREF_LOAD_TOURNAMENT_SORT_KEY, Sort.CREATION_DATE_NEWEST.name) ?: Sort.CREATION_DATE_NEWEST.name).let {
-            try {
-                Sort.valueOf(it)
-            } catch (e: java.lang.IllegalArgumentException) {
-                Sort.CREATION_DATE_NEWEST
-            }
+        try {
+            Sort.valueOf(
+                sharedPreferences.getString(
+                    PREF_LOAD_TOURNAMENT_SORT_KEY,
+                    Sort.CREATION_DATE_NEWEST.name
+                ) ?: Sort.CREATION_DATE_NEWEST.name
+            )
+        } catch (e: java.lang.IllegalArgumentException) {
+            Sort.CREATION_DATE_NEWEST
         }
 
-    override fun setSort(sort: Sort) {
-        val editor = sharedPreferences.edit()
-        editor.putString(PREF_LOAD_TOURNAMENT_SORT_KEY, sort.name)
-        editor.apply()
-    }
+
+    override fun setSort(sort: Sort) =
+        sharedPreferences.edit().run {
+            putString(PREF_LOAD_TOURNAMENT_SORT_KEY, sort.name)
+            apply()
+        }
+
 
 }
