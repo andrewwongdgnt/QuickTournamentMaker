0b4a19f37cd876356b9380a2dee3ea44cfc71757
Andrew Wong <andrew.wong.dgnt@gmail.com>
2022-02-06 23:07:07 -0800

    the date edit texts will display a date picker now

--- app/src/main/java/com/dgnt/quickTournamentMaker/ui/main/loadTournament/LoadTournamentFilterOptionsDialogFragment.kt
+++ app/src/main/java/com/dgnt/quickTournamentMaker/ui/main/loadTournament/LoadTournamentFilterOptionsDialogFragment.kt
@@ -1,14 +1,18 @@
 package com.dgnt.quickTournamentMaker.ui.main.loadTournament
 
 import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import com.dgnt.quickTournamentMaker.R
 import com.dgnt.quickTournamentMaker.databinding.LoadTournamentFilterOptionsFragmentBinding
 import com.dgnt.quickTournamentMaker.model.tournament.TournamentType
 import com.dgnt.quickTournamentMaker.ui.main.common.OnEditListener
 import com.dgnt.quickTournamentMaker.util.toIntOr
+import org.joda.time.LocalDateTime
+import org.joda.time.format.DateTimeFormat
 import org.kodein.di.DIAware
 import org.kodein.di.android.x.di
 import org.kodein.di.instance
@@ -43,6 +47,31 @@ class LoadTournamentFilterOptionsDialogFragment(
             binding.lifecycleOwner = this
 
             viewModel.init()
+
+            binding.apply {
+                val dateFormat = DateTimeFormat.mediumDate()
+
+                val invokeDatePickerDialog = { mutableLiveDataDate: MutableLiveData<String>,
+                                               mutableLiveDataDateBacking: MutableLiveData<LocalDateTime> ->
+                    mutableLiveDataDateBacking.value?.let { passedInDate ->
+                        DatePickerDialog(
+                            activity,
+                            { _, year, monthOfYear, dayOfMonth ->
+                                val date = LocalDateTime(year, monthOfYear + 1, dayOfMonth, 0, 0)
+                                mutableLiveDataDate.value = dateFormat.print(date)
+                                mutableLiveDataDateBacking.value = date
+                            },
+                            passedInDate.year, passedInDate.monthOfYear - 1, passedInDate.dayOfMonth
+                        ).show()
+                    }
+                }
+
+                earliestCreatedDateEt.setOnClickListener { invokeDatePickerDialog(viewModel.earliestCreatedDate, viewModel.earliestCreatedDateBacking) }
+                latestCreatedDateEt.setOnClickListener { invokeDatePickerDialog(viewModel.latestCreatedDate, viewModel.latestCreatedDateBacking) }
+                earliestModifiedDateEt.setOnClickListener { invokeDatePickerDialog(viewModel.earliestModifiedDate, viewModel.earliestModifiedDateBacking) }
+                latestModifiedDateEt.setOnClickListener { invokeDatePickerDialog(viewModel.latestModifiedDate, viewModel.latestModifiedDateBacking) }
+            }
+
             return AlertDialog.Builder(activity)
                 .setTitle(getString(R.string.filter))
                 .setView(binding.root)
