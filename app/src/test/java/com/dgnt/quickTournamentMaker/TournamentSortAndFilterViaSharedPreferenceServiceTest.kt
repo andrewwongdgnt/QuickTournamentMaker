aa59fd3ba6d5578b3bd2ca6a91036f130c345ed0
Andrew Wong <andrew.wong.dgnt@gmail.com>
2022-02-11 22:37:57 -0800

    added more unit tests. DIdnt do dates yet though

--- app/src/test/java/com/dgnt/quickTournamentMaker/TournamentSortAndFilterViaSharedPreferenceServiceTest.kt
+++ app/src/test/java/com/dgnt/quickTournamentMaker/TournamentSortAndFilterViaSharedPreferenceServiceTest.kt
@@ -192,7 +192,7 @@ class TournamentSortAndFilterViaSharedPreferenceServiceTest {
                 TournamentInformation(
                     "title10",
                     "description10",
-                    TournamentType.ELIMINATION,
+                    TournamentType.SURVIVAL,
                     SeedType.CUSTOM,
                     RankPriorityConfig.DEFAULT,
                     LocalDateTime(2022, 6, 6, 6, 59, 10)
@@ -206,11 +206,8 @@ class TournamentSortAndFilterViaSharedPreferenceServiceTest {
         )
 
         list = listOf()
-    }
 
-    @Test
-    fun testNoFilter() {
-
+        // Everything disabled to start
         listOf(TournamentType.ELIMINATION, TournamentType.DOUBLE_ELIMINATION, TournamentType.ROUND_ROBIN, TournamentType.SWISS, TournamentType.SURVIVAL).forEach {
             PowerMockito.`when`(mockPreferenceService.isFilteredOnTournamentType(it)).thenReturn(false)
         }
@@ -223,6 +220,10 @@ class TournamentSortAndFilterViaSharedPreferenceServiceTest {
 
         PowerMockito.`when`(mockPreferenceService.isFilteredOnEarliestModifiedDate()).thenReturn(false)
         PowerMockito.`when`(mockPreferenceService.isFilteredOnLatestModifiedDate()).thenReturn(false)
+    }
+
+    @Test
+    fun testNoFilter() {
 
         Assert.assertEquals(listOf(tournament1, tournament2, tournament3, tournament4, tournament5, tournament6, tournament7, tournament8, tournament9, tournament10), sut.update(list))
 
@@ -230,19 +231,6 @@ class TournamentSortAndFilterViaSharedPreferenceServiceTest {
 
     @Test
     fun testNoFilterButWithValuesAnyway() {
-
-        listOf(TournamentType.ELIMINATION, TournamentType.DOUBLE_ELIMINATION, TournamentType.ROUND_ROBIN, TournamentType.SWISS, TournamentType.SURVIVAL).forEach {
-            PowerMockito.`when`(mockPreferenceService.isFilteredOnTournamentType(it)).thenReturn(false)
-        }
-
-        PowerMockito.`when`(mockPreferenceService.isFilteredOnMinimumParticipants()).thenReturn(false)
-        PowerMockito.`when`(mockPreferenceService.isFilteredOnMaximumParticipants()).thenReturn(false)
-
-        PowerMockito.`when`(mockPreferenceService.isFilteredOnEarliestCreatedDate()).thenReturn(false)
-        PowerMockito.`when`(mockPreferenceService.isFilteredOnLatestCreatedDate()).thenReturn(false)
-
-        PowerMockito.`when`(mockPreferenceService.isFilteredOnEarliestModifiedDate()).thenReturn(false)
-        PowerMockito.`when`(mockPreferenceService.isFilteredOnLatestModifiedDate()).thenReturn(false)
 
         //test that these are ignored
         PowerMockito.`when`(mockPreferenceService.getMinimumParticipantsToFilterOn()).thenReturn(Int.MAX_VALUE)
@@ -255,4 +243,65 @@ class TournamentSortAndFilterViaSharedPreferenceServiceTest {
         Assert.assertEquals(listOf(tournament1, tournament2, tournament3, tournament4, tournament5, tournament6, tournament7, tournament8, tournament9, tournament10), sut.update(list))
 
     }
+
+    @Test
+    fun testEliminationOnly() {
+        PowerMockito.`when`(mockPreferenceService.isFilteredOnTournamentType(TournamentType.ELIMINATION)).thenReturn(true)
+        Assert.assertEquals(listOf(tournament1, tournament2, tournament9), sut.update(list))
+    }
+
+    @Test
+    fun testDoubleEliminationOnly() {
+        PowerMockito.`when`(mockPreferenceService.isFilteredOnTournamentType(TournamentType.DOUBLE_ELIMINATION)).thenReturn(true)
+        Assert.assertEquals(listOf(tournament6, tournament7), sut.update(list))
+    }
+
+    @Test
+    fun testRoundRobinOnly() {
+        PowerMockito.`when`(mockPreferenceService.isFilteredOnTournamentType(TournamentType.ROUND_ROBIN)).thenReturn(true)
+        Assert.assertEquals(listOf(tournament3, tournament8), sut.update(list))
+    }
+
+    @Test
+    fun testSwissOnly() {
+        PowerMockito.`when`(mockPreferenceService.isFilteredOnTournamentType(TournamentType.SWISS)).thenReturn(true)
+        Assert.assertEquals(listOf(tournament4, tournament5), sut.update(list))
+    }
+
+    @Test
+    fun testSurvivalOnly() {
+        PowerMockito.`when`(mockPreferenceService.isFilteredOnTournamentType(TournamentType.SURVIVAL)).thenReturn(true)
+        Assert.assertEquals(listOf(tournament10), sut.update(list))
+    }
+
+    @Test
+    fun testAllTournamentTypeFilter() {
+        listOf(TournamentType.ELIMINATION, TournamentType.DOUBLE_ELIMINATION, TournamentType.ROUND_ROBIN, TournamentType.SWISS, TournamentType.SURVIVAL).forEach {
+            PowerMockito.`when`(mockPreferenceService.isFilteredOnTournamentType(it)).thenReturn(true)
+        }
+
+        // same outcome as no filter
+        Assert.assertEquals(listOf(tournament1, tournament2, tournament3, tournament4, tournament5, tournament6, tournament7, tournament8, tournament9, tournament10), sut.update(list))
+
+    }
+
+    @Test
+    fun testMin6Participants() {
+        PowerMockito.`when`(mockPreferenceService.isFilteredOnMinimumParticipants()).thenReturn(true)
+        PowerMockito.`when`(mockPreferenceService.getMinimumParticipantsToFilterOn()).thenReturn(6)
+
+        Assert.assertEquals(listOf(tournament1, tournament2, tournament3, tournament9, tournament10), sut.update(list))
+
+    }
+
+    @Test
+    fun testMax5Participants() {
+        PowerMockito.`when`(mockPreferenceService.isFilteredOnMaximumParticipants()).thenReturn(true)
+        PowerMockito.`when`(mockPreferenceService.getMaximumParticipantsToFilterOn()).thenReturn(5)
+
+        Assert.assertEquals(listOf(tournament4, tournament5, tournament6, tournament7, tournament8), sut.update(list))
+
+    }
+
+    //TODO test for dates
 }
