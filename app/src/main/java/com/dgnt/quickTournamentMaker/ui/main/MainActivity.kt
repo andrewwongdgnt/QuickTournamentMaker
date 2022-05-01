793bb34e9d819c514547495811cc6cdec812a34b
Andrew Wong <andrew.wong.dgnt@gmail.com>
2022-05-01 13:02:05 -0700

    FIx main fragments from overlapping with each other on rotate and dark mode/light mode change - needed to check if these fragments were already added and dont do it if true

--- app/src/main/java/com/dgnt/quickTournamentMaker/ui/main/MainActivity.kt
+++ app/src/main/java/com/dgnt/quickTournamentMaker/ui/main/MainActivity.kt
@@ -1,48 +1,65 @@
 package com.dgnt.quickTournamentMaker.ui.main
 
 import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.dgnt.quickTournamentMaker.R
+import com.dgnt.quickTournamentMaker.databinding.MainActivityBinding
 import com.dgnt.quickTournamentMaker.ui.main.home.HomeFragment
 import com.dgnt.quickTournamentMaker.ui.main.loadTournament.LoadTournamentFragment
 import com.dgnt.quickTournamentMaker.ui.main.management.ManagementFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+
+private const val HOME_FRAGMENT_TAG = "HOME_FRAGMENT_TAG"
+private const val MANAGEMENT_FRAGMENT_TAG = "MANAGEMENT_FRAGMENT_TAG"
+private const val LOAD_TOURNAMENT_FRAGMENT_TAG = "LOAD_TOURNAMENT_FRAGMENT_TAG"
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        MainActivityBinding.inflate(layoutInflater).run {
+            setContentView(root)
 
-        val homeFragment = HomeFragment.newInstance()
-        val managementFragment = ManagementFragment.newInstance()
-        val loadTournamentFragment = LoadTournamentFragment.newInstance()
-        var activeFragment: Fragment = homeFragment
-        supportFragmentManager.beginTransaction().apply {
-            add(R.id.main_fragment_container, homeFragment)
-            add(R.id.main_fragment_container, managementFragment).hide(managementFragment)
-            add(R.id.main_fragment_container, loadTournamentFragment).hide(loadTournamentFragment)
+            val homeFragment = supportFragmentManager.findFragmentByTag(HOME_FRAGMENT_TAG) ?: HomeFragment.newInstance()
+            val managementFragment = supportFragmentManager.findFragmentByTag(MANAGEMENT_FRAGMENT_TAG) ?: ManagementFragment.newInstance()
+            val loadTournamentFragment = supportFragmentManager.findFragmentByTag(LOAD_TOURNAMENT_FRAGMENT_TAG) ?: LoadTournamentFragment.newInstance()
+            supportFragmentManager.beginTransaction().apply {
+                addIfNotExists(mainFragmentContainer.id, homeFragment, HOME_FRAGMENT_TAG)
+                addIfNotExists(mainFragmentContainer.id, managementFragment, MANAGEMENT_FRAGMENT_TAG)?.hide(managementFragment)
+                addIfNotExists(mainFragmentContainer.id, loadTournamentFragment, LOAD_TOURNAMENT_FRAGMENT_TAG)?.hide(loadTournamentFragment)
+            }.commit()
 
-        }.commit()
+            navView.setOnItemSelectedListener { item ->
+                val fragment = when (item.itemId) {
+                    R.id.navigation_home -> homeFragment
+                    R.id.navigation_management -> managementFragment
+                    R.id.navigation_loadTournament -> loadTournamentFragment
+                    else -> homeFragment
+                }
+                supportFragmentManager
+                    .beginTransaction()
+                    .hide(homeFragment)
+                    .hide(managementFragment)
+                    .hide(loadTournamentFragment)
+                    .show(fragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                    .commit()
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        navView.setOnNavigationItemSelectedListener { item ->
-            val fragment = when (item.itemId) {
-                R.id.navigation_home -> homeFragment
-                R.id.navigation_management -> managementFragment
-                R.id.navigation_loadTournament -> loadTournamentFragment
-                else -> homeFragment
+                true
             }
-            supportFragmentManager.beginTransaction().hide(activeFragment).show(fragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit()
-            activeFragment = fragment
-
-            true
         }
-
     }
+
+    private fun FragmentTransaction.addIfNotExists(
+        @IdRes containerViewId: Int, fragment: Fragment,
+        @Nullable tag: String
+    ) =
+        if (supportFragmentManager.findFragmentByTag(tag) == null)
+            add(containerViewId, fragment, tag)
+        else null
 
 
 }
