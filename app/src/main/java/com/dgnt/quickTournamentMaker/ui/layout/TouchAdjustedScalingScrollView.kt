29486c650d60bbb636bb11e476006a24c7dd8e13
Andrew Wong <andrew.wong.dgnt@gmail.com>
2022-05-13 23:32:48 -0700

    Handle panning for scaling scroll view but on certain scales, the screen flickers when moving

--- app/src/main/java/com/dgnt/quickTournamentMaker/ui/layout/TouchAdjustedScalingScrollView.kt
+++ app/src/main/java/com/dgnt/quickTournamentMaker/ui/layout/TouchAdjustedScalingScrollView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Matrix
 import android.util.AttributeSet
 import android.view.MotionEvent
+import com.dgnt.quickTournamentMaker.util.SimpleLogger
 import com.moagrius.widget.ScalingScrollView
 import com.moagrius.widget.ScalingScrollView.ScaleChangedListener
 
@@ -15,6 +16,7 @@ class TouchAdjustedScalingScrollView : ScalingScrollView {
 
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
 
+    private var hasSingleFingerDown: Boolean = false
     private var currentScale = 1.0f
 
     private val mScaleChangedListener = ScaleChangedListener { _, currentScale, previousScale ->
@@ -25,21 +27,35 @@ class TouchAdjustedScalingScrollView : ScalingScrollView {
         scaleChangedListener = mScaleChangedListener
     }
 
-
-    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
-        val matrix = Matrix()
-        val mappingMatrix = Matrix()
-        //TODO handle panning
-        matrix.setScale(currentScale, currentScale)
-        matrix.invert(mappingMatrix)
-
-        val transformEvent = MotionEvent.obtain(event)
-        transformEvent.transform(mappingMatrix)
-
-        val returnVal = super.dispatchTouchEvent(transformEvent)
-
-        transformEvent.recycle()
-        return returnVal
+    override fun onPointerCounterChange(pointerCount: Int) {
+        hasSingleFingerDown = pointerCount == 1
+        super.onPointerCounterChange(pointerCount)
     }
 
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+
+        SimpleLogger.d(this, "action: ${event.action}")
+        if (hasSingleFingerDown) {
+            val matrix = Matrix()
+            val mappingMatrix = Matrix()
+            matrix.setScale(currentScale, currentScale)
+//            matrix.postTranslate(-scrollX*(1-scale), -scrollY*(1-scale))
+            matrix.postTranslate(-scrollX*(1-scale), -scrollY*(1-scale))
+
+            matrix.invert(mappingMatrix)
+            SimpleLogger.d(this, "x: $scrollX, y: $scrollY")
+//            SimpleLogger.d(this, mappingMatrix.toShortString())
+
+            val transformEvent = MotionEvent.obtain(event)
+            transformEvent.transform(mappingMatrix)
+
+            val returnVal = super.dispatchTouchEvent(transformEvent)
+
+            transformEvent.recycle()
+            return returnVal
+        } else {
+            SimpleLogger.d(this, "x: $scrollX, y: $scrollY")
+            return super.dispatchTouchEvent(event)
+        }
+    }
 }
